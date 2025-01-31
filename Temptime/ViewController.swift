13d55d9b72391c49bfe762e35800995cb6e98eb0
stevenyu253436 <stevenyu.supreme@gmail.com>
2025-01-31 18:23:20 +0800

    Initial Commit

---
+++ Temptime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Temptime
+//
+//  Created by 游哲維 on 2025/1/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TemptimeTests/TemptimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TemptimeTests.swift
+//  TemptimeTests
+//
+//  Created by 游哲維 on 2025/1/31.
+//
+
+import Testing
+@testable import Temptime
+
+struct TemptimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
